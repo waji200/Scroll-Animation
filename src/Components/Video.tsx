d2f3b4e9e8aef5f5ae5bd294b573f5e905bc1bbd
Wajahat Baig <waji.baig11@gmail.com>
2023-08-07 01:56:48 +0500

    performance check2

--- src/Components/Video.tsx
+++ src/Components/Video.tsx
@@ -62,50 +62,74 @@ const Video: React.FC<VideoProps> = ({ progress, imageSequenceSrc }) => {
     };
 
     preloadImages();
-  }, [progress, numFrames, imageSequenceSrc, isImageLoaded]);
+  }, [progress, numFrames, imageSequenceSrc]);
 
   useEffect(() => {
-    // if (!isImageLoaded || !currentImage) return;
-
     let animationFrameId: number;
 
-    const updateImage = () => {
-      if (isImageLoaded || currentImage) {
-      animationFrameId = requestAnimationFrame(updateImage);
-      setCurrentImage(null);
+    const updateCanvas = () => {
+      if (isImageLoaded && currentImage) {
+        const canvas = canvasRef.current;
+        const context = canvas?.getContext('2d');
+
+        if (context) {
+          const canvasWidth = canvas!.parentElement!.clientWidth;
+          const canvasHeight = canvas!.parentElement!.clientHeight;
+
+          context.clearRect(0, 0, canvasWidth, canvasHeight);
+
+          const imageAspectRatio = currentImage.width / currentImage.height;
+          const canvasAspectRatio = canvasWidth / canvasHeight;
+
+          let drawWidth = currentImage.width;
+          let drawHeight = currentImage.height;
+
+          if (imageAspectRatio > canvasAspectRatio) {
+            drawHeight = canvasHeight;
+            drawWidth = canvasHeight * imageAspectRatio;
+          } else {
+            drawWidth = canvasWidth;
+            drawHeight = canvasWidth / imageAspectRatio;
+          }
+
+          const offsetX = (canvasWidth - drawWidth) / 2;
+          const offsetY = (canvasHeight - drawHeight) / 2;
+
+          context.drawImage(currentImage, offsetX, offsetY, drawWidth, drawHeight);
+        }
+      }
+
+      animationFrameId = requestAnimationFrame(updateCanvas);
+    };
+
+    if (isImageLoaded && currentImage) {
+      animationFrameId = requestAnimationFrame(updateCanvas);
     }
-    }
-    if (isImageLoaded || currentImage) {
-    updateImage();
-    }
+
     return () => {
       cancelAnimationFrame(animationFrameId);
     };
-  }, [progress, numFrames, isImageLoaded, currentImage]);
+  }, [isImageLoaded, currentImage]);
 
   useEffect(() => {
-    // if (!isImageLoaded || !currentImage) return;
-
     // Show text overlay for 5 seconds when the image sequence starts
     if (textRef.current) {
       if (progress > 70 / numFrames && progress < 90 / numFrames) {
         gsap.to(textRef.current, { autoAlpha: 1, duration: 0.5, scale: '1', display: 'block' });
+      } else if (progress > 90 / numFrames) {
+        gsap.to(textRef.current, { autoAlpha: 0, duration: 0.5, scale: '2', display: 'hidden' });
       } else {
-        if (progress > 90 / numFrames) {
-          gsap.to(textRef.current, { autoAlpha: 0, duration: 0.5, scale: '2', display: 'hidden' });
-        } else {
-          gsap.to(textRef.current, { autoAlpha: 0, duration: 0.5, scale: '0.3', display: 'hidden' });
-        }
+        gsap.to(textRef.current, { autoAlpha: 0, duration: 0.5, scale: '0.3', display: 'hidden' });
       }
     }
-  }, [progress, numFrames, isImageLoaded, currentImage]);
+  }, [progress, numFrames]);
 
   return (
-    <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+    <div style={{ width: '100%', height: '100%', position: 'relative', overflow: 'hidden' }}>
       {/* Text Overlay */}
       <div
         ref={textRef}
-        className='absolute hidden top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white font-bold text-center'
+        className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white font-bold text-center'
       >
         <h1 className='text-white text-6xl drop-shadow-[0_0_3px_rgba(255,255,255,1)]'>JAY L</h1>
         <p className='text-xl text-yellow-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ullam illum quasi repellendus itaque in.</p>
