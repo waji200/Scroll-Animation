42beb1251179d8435e91cf3f3f4612a4bc803255
Wajahat Baig <waji.baig11@gmail.com>
2023-08-08 03:44:41 +0500

    card cleared

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,17 +1,17 @@
-import demoImage from '../assets/images/demo-1.png'
+
 
 function Cards() {
   return (
     
   <div className="max-w-xl">
       <a href="#">
-          <img className="" src={demoImage} alt="" />
+          <img className="bg-white w-[100%] h-[25rem]" src='' alt="" />
       </a>
       <div className="py-5">
           <a href="#">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Heading</h5>
           </a>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis, molestiae.</p>
           <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Read more
               <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
